059bd698b4e5d24018f5ad5c77d0c8ded80df094
povarintsev-av <difkust@mail.ru>
2025-03-04 00:17:10 +1000

    123

--- vars/OtusLibrary.groovy
+++ vars/OtusLibrary.groovy
@@ -1,18 +1,31 @@
 
 class OtusLibrary {
 
-    boolean checkAnsible() {
-        
-        boolean isInstalled;
-        byte rest = sh returnStatus: true, script: 'ansible --version'
-        
-        if (rest == 0) {
-            isInstalled = true
-        } else {
-            isInstalled = false
+    def checkAnsible() {
+        try {
+            def process = new ProcessBuilder("ansible", "--version").start()
+            def exitCode = process.waitFor()
+            
+            // Получаем вывод команды
+            def output = new String(process.inputStream.readAllBytes())
+            def error = new String(process.errorStream.readAllBytes())
+            
+            println("\033[38;2;138;43;226m[OtusLibrary] Ansible check output: ${output}\033[0m")
+            
+            if (exitCode != 0) {
+                println("\033[38;2;255;0;0m[OtusLibrary] Ansible check error: ${error}\033[0m")
+                println("\033[38;2;255;0;0m[OtusLibrary] Exit code: ${exitCode}\033[0m")
+                return false
+            }
+            
+            return true
+        } catch (Exception e) {
+            println("\033[38;2;255;0;0m[OtusLibrary] Error checking Ansible: ${e.getMessage()}\033[0m")
+            return false
         }
-        
-        return isInstalled;
+    }
 
+    def call() {
+        return this
     }
 }
