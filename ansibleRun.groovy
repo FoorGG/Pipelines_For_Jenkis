d419942fd49e9f1c7939eb810780e3606d595c16
povarintsev-av <difkust@mail.ru>
2025-03-03 22:05:28 +1000

    Testing

--- ansibleRun.groovy
+++ ansibleRun.groovy
@@ -1,18 +1,30 @@
-def str = env.STRING
+// String ansible_playbook = env.ANSIBLE_PLAYBOOK
+// String ansible_inventory = env.ANSIBLE_INVENTORY
+// String ansible_user = env.ANSIBLE_USER
+// String ansible_password = env.ANSIBLE_PASSWORD
+// String ansible_ssh_key = env.ANSIBLE_SSH_KEY
+String git_url = env.GIT_URL
+String git_branch = env.GIT_BRANCH
 
 pipeline{
 
     agent any
 
+    options {
+        ansiColor('xterm')
+    }
+
     stages{
-        stage('print'){
+        stage('checkout'){
             steps{
-                script{
-                    println("${str}")
+                
+                println("\032[32mChecking out ${git_url} ${git_branch}\033[0m")
+
                 }
             }
         }
     }
+
     post {
         always {
             deleteDir()
@@ -21,4 +33,5 @@ pipeline{
             }
         }
     }
+
 }
