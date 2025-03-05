509b46c24e4412888c9524f972c2a13ab48c0d88
povarintsev-av <difkust@mail.ru>
2025-03-05 23:01:40 +1000

    123

--- ansibleRun.groovy
+++ ansibleRun.groovy
@@ -77,7 +77,7 @@ pipeline {
                             'ansible-playbook',
                             "ansible/${ansible_playbook}",
                             "-i ansible/${ansible_inventory}",
-                            "-e \"ansible_password=123456789 ansible_become_password=123456789\""
+                            "-e \"ansible_password=${ansible_password} ansible_become_password=${ansible_password}\""
                         ]
                         
                         sh "echo '\033[38;2;138;43;226m[Pipeline] Starting Ansible playbook execution...\033[0m'"
